21fba5e69e2b47b6229caa724c28ab7be60864ea
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 16:38:28 +0900

    메뉴 완료

--- food_menu.py
+++ food_menu.py
@@ -3,6 +3,7 @@ food_menu = [
     {"2번 세트":["고등어구이", "불고기", "비빔밥"]},
     {"3번 세트":["샌드위치", "샐러드"]}
     ]
+print("--- 도시락 메뉴 구성 ---")
 
 for i in range(len(food_menu)):
     food_set_menu = food_menu[i]
@@ -13,4 +14,7 @@ for i in range(len(food_menu)):
         for z in range(len(food_menu_in_menu)):
             setmenu_indi = food_menu_in_menu[z]
 
-            print(setmenu_indi)
+            print(f"세트 {i}의 메뉴 {z}번: {setmenu_indi}")
+
+    print("----------------")
+print("오늘의 메뉴 구성이 완료되었습니다.")
