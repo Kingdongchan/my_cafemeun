ec3c23c8b23cd69388b2363299fd35085fdf42dc
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 15:23:18 +0900

    __str__ 실습

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ class cafeMenu:
         self.cafe_food = name
         self.cafe_food_price = price
 
-    def order_output(self):
-        print(f"주문하신 {self.cafe_food} 나왔습니다")
+    def __str__(self):
+        
+        return f"주문하신 {self.cafe_food} 나왔습니다"
 
 americano = cafeMenu("아메리카노", 5000)
 cafelatte = cafeMenu("카페라떼", 5500)
 bananalatte = cafeMenu("바닐라라뗴", 6000)
 
-americano.order_output()
-cafelatte.order_output()
-bananalatte.order_output()
+print(americano)
+print(cafelatte)
