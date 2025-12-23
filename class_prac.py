88db42fabf44955f22a2f2b3fb470d106dbb3646
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 15:54:27 +0900

    출력 작성 완료

--- class_prac.py
+++ class_prac.py
@@ -2,3 +2,9 @@ class book:
     def __init__ (self, title, author):
         self.book_title = title
         self.book_author = author
+
+    def __str__(self):
+        
+        return f"이 책의 제목은 {self.book_title}이고, 저자는 {self.book_author}입니다."
+    
+
