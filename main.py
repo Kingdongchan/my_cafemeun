cab20759ceca959f66b7a81fe509d52a58fccccf
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 14:54:33 +0900

    __init__ 수정

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class cafeMenu:
-    def order (self, name, price):
+    def __inti__ (self, name, price):
         cafe_food = name
         cafe_food_price = price
 
         def order_output(self):
-         print(f"주문하신 {cafe_food} 나왔습니다.")
+         print(f"주문하신 {cafe_food} 나왔습니다. 가격은 {cafe_food_price}입니다.")
 
 americano = cafeMenu("아메리카노", 5000)
 cafelatte = cafeMenu("카페라떼", 5500)
